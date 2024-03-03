0340d524dd47f08ae94bc79434cb1fa2d22719ff
SalmaElMarhoumi <salmaelmarhoumi2000@gmail.com>
2024-03-03 14:51:42 +0200

    react-lab2

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import Apitask from './Components/Apitask';
+function App() {
+  return (
+    <div className="App">
+      <Apitask/>
+    </div>
+  );
+}
+
+export default App;
